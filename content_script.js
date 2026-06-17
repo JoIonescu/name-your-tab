f37d628d84fed6dc53f92732000dece012fe60cb
Jo_dev <157588282+JoIonescu@users.noreply.github.com>
2026-06-17 18:19:03 +0200

    Refactor title update logic and observer registration

--- content_script.js
+++ content_script.js
@@ -3,38 +3,32 @@ function updateTitle(tabIndex) {
     return;
   }
   if (typeof window.lastTitle === "undefined" || document.title != window.lastTitle) {
-    // Title has never been adjusted or has been changed by something other than this script.
     window.originalTitle = document.title;
     setTitle(document.title, tabIndex);
   } else if (tabIndex != window.lastTabIndex) {
-    // Tab index has changed; reuse last title.
     setTitle(window.originalTitle, tabIndex);
   }
 }
-
 function setTitle(title, tabIndex) {
   var prefix = tabIndex < 8 ? (tabIndex + 1) + ' ' : '';
   document.title = prefix + title + ' <' + location.host + '>';
   window.lastTitle = document.title;
   window.lastTabIndex = tabIndex;
 }
-
 function registerListener() {
-  // Listen for changes from service_worker.js.
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       updateTitle(request.tabIndex);
     });
-
-  // Listen for changes to the title originating from within the page.
   var titleObserver = new MutationObserver(function(mutations) {
     updateTitle(window.lastTabIndex);
   });
-  
   var config = { attributes: true, childList: true, characterData: true };
-  titleObserver.observe(document.getElementsByTagName('title')[0], config);
+  var titleEl = document.getElementsByTagName('title')[0];
+  if (titleEl) {
+    titleObserver.observe(titleEl, config);
+  }
 }
-
 document.addEventListener("DOMContentLoaded", function(event) {
   registerListener();
 });

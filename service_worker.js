d206cb98ec4122412eb5a0e92f2cbd3ba1f72564
Jo_dev <157588282+JoIonescu@users.noreply.github.com>
2026-06-17 18:32:08 +0200

    Refactor tab update logic in service worker

--- service_worker.js
+++ service_worker.js
@@ -1,22 +1,29 @@
 console.log("service worker loaded");
 
 function updateTabName(tab) {
-  console.log("updateTabName called for tab:", tab.id, "index:", tab.index);
-  chrome.tabs.sendMessage(tab.id, {"tabIndex": tab.index}).catch(error => {
-    console.log("sendMessage error:", error);
-  });
+  if (!tab.id) return;
+  chrome.tabs.sendMessage(tab.id, {"tabIndex": tab.index}).catch(() => {});
 }
+
 function updateAllTabs() {
   chrome.tabs.query({}, function(tabs) {
-    console.log("updateAllTabs - found tabs:", tabs.length);
-    for(var i = 0; i < tabs.length; i++) {
+    console.log("updating all tabs:", tabs.length);
+    for (var i = 0; i < tabs.length; i++) {
       updateTabName(tabs[i]);
     }
   });
 }
-chrome.tabs.onCreated.addListener(function() { updateAllTabs(); });
-chrome.tabs.onMoved.addListener(function() { updateAllTabs(); });
-chrome.tabs.onRemoved.addListener(function() { updateAllTabs(); });
-chrome.tabs.onDetached.addListener(function() { updateAllTabs(); });
-chrome.tabs.onAttached.addListener(function() { updateAllTabs(); });
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { updateTabName(tab); });
+
+// Run immediately when service worker wakes
+updateAllTabs();
+
+chrome.tabs.onCreated.addListener(updateAllTabs);
+chrome.tabs.onMoved.addListener(updateAllTabs);
+chrome.tabs.onRemoved.addListener(updateAllTabs);
+chrome.tabs.onDetached.addListener(updateAllTabs);
+chrome.tabs.onAttached.addListener(updateAllTabs);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete") {
+    updateAllTabs();
+  }
+});

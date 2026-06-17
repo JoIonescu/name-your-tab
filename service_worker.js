fbde842731f389a35c9654349fd26cca184253ef
Jo_dev <157588282+JoIonescu@users.noreply.github.com>
2026-06-17 19:06:25 +0200

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -2,14 +2,13 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
     chrome.storage.local.get([String(tabId)], (res) => {
       const data = res[String(tabId)];
-      if (data?.name) {
-        chrome.tabs.sendMessage(tabId, { type: "apply", name: data.name }).catch(() => {});
+      if (data) {
+        chrome.tabs.sendMessage(tabId, { type: "apply", name: data.name, color: data.color }).catch(() => {});
       }
     });
   }
 });
 
-// When tab is closed, clean up storage
 chrome.tabs.onRemoved.addListener((tabId) => {
   chrome.storage.local.remove(String(tabId));
 });

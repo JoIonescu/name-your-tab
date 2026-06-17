9791b78b1c9ace1b1b1544e530409b4608833cab
Jo_dev <157588282+JoIonescu@users.noreply.github.com>
2026-06-17 18:23:05 +0200

    Refactor tab event listeners and logging
    Refactor event listeners for tab updates and streamline logging.

--- service_worker.js
+++ service_worker.js
@@ -1,29 +1,22 @@
+console.log("service worker loaded");
+
 function updateTabName(tab) {
-  chrome.tabs.sendMessage(tab.id, {"tabIndex": tab.index}).catch(error => {});
+  console.log("updateTabName called for tab:", tab.id, "index:", tab.index);
+  chrome.tabs.sendMessage(tab.id, {"tabIndex": tab.index}).catch(error => {
+    console.log("sendMessage error:", error);
+  });
 }
 function updateAllTabs() {
   chrome.tabs.query({}, function(tabs) {
+    console.log("updateAllTabs - found tabs:", tabs.length);
     for(var i = 0; i < tabs.length; i++) {
-      var tab = tabs[i];
-      updateTabName(tab);
+      updateTabName(tabs[i]);
     }
   });
 }
-chrome.tabs.onCreated.addListener(function() {
-  updateAllTabs();
-});
-chrome.tabs.onMoved.addListener(function() {
-  updateAllTabs();
-});
-chrome.tabs.onRemoved.addListener(function() {
-  updateAllTabs();
-});
-chrome.tabs.onDetached.addListener(function() {
-  updateAllTabs();
-});
-chrome.tabs.onAttached.addListener(function() {
-  updateAllTabs();
-});
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  updateTabName(tab);
-});
+chrome.tabs.onCreated.addListener(function() { updateAllTabs(); });
+chrome.tabs.onMoved.addListener(function() { updateAllTabs(); });
+chrome.tabs.onRemoved.addListener(function() { updateAllTabs(); });
+chrome.tabs.onDetached.addListener(function() { updateAllTabs(); });
+chrome.tabs.onAttached.addListener(function() { updateAllTabs(); });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { updateTabName(tab); });
